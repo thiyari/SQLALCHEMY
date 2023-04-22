547d1e1c4edda5d9674c31fa2c35391aa235997c
Manikanth <manikanth578@gmail.com>
2023-04-22 17:24:49 +0530

    first commit

---
+++ retrieving.py
@@ -0,0 +1,16 @@
+from main import User,Session,engine
+
+local_session=Session(bind=engine)
+
+#return all objects
+#users=local_session.query(User).all()[:3]
+#for user in users:
+#	print(user.username)
+
+#query for one object
+
+jona=local_session.query(User).filter(User.username=="jona").first()
+print(jona)
+
+
+
